dd1d7d04e7a2209f06e5fced3f39e711bd64a21d
aleks6699 <58092216+aleks6699@users.noreply.github.com>
2020-01-24 13:54:14 +0200

    Add files via upload

---
+++ business/js/main.js
@@ -0,0 +1,53 @@
+$(function() {
+  $(".intro__slider").slick({
+    infinite: true,
+    fade: true,
+    dots: true,
+    prevArrow:
+      '<img class="slider-arrows slider-arrows_left" src="./img/arrows-left.png" alt=""></img>',
+    nextArrow:
+      '<img class="slider-arrows slider-arrows_right" src="./img/arrows-right.png" alt=""></img>',
+    responsive: [
+      {
+        breakpoint: 655,
+        settings: {
+          arrows: false
+        }
+      }
+    ]
+  });
+
+  $(".testimonials__slider").slick({
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    dots: true,
+    infinite: true,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 902,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+
+  $(".burger").on("click", function() {
+    $(".menu, .burger").toggleClass("active");
+  });
+
+  $("[data-scroll]").on("click", function(event) {
+    event.preventDefault(); // отменяет стандартое поведение ссылки
+    let elementID = $(this).data("scroll");
+    let elementOffset = $(elementID).offset().top; // отступ данного елемента от верха
+
+    $("html, body").animate(
+      {
+        scrollTop: elementOffset - 70
+      },
+      1000
+    );
+  });
+});
